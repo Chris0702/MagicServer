fa0e7fd0717cc543c81e0ef6f25b375f7974e267
Jie <s_213456789@yahoo.com.tw>
2016-07-17 22:45:53 +0800

    add chat.html can read

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,6 +1,13 @@
 module.exports = function(server) {
-  // Install a `/` route that returns server status
-  var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
-  server.use(router);
+    // Install a `/` route that returns server status
+    var router = server.loopback.Router();
+    var path = require('path');
+
+    router.get('/', server.loopback.status());
+    router.get('/chat', function(req, res) {
+        console.log('send chat.html');
+        res.sendFile(path.join(__dirname, '../../client/chat.html'));
+    });
+     server.use(router);
+
 };
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var loopback = require('loopback');
 var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
-
+//app.asd='zzzzzzz';
 app.start = function() {
   // start the web server
   return app.listen(function() {
