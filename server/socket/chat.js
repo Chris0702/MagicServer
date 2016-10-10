31079d64d3970771d22804a60858b9ea63d45095
Chris <s_213456789@yahoo.com.tw>
2016-10-10 23:16:22 +0800

    tmp

--- server/socket/chat.js
+++ server/socket/chat.js
@@ -92,21 +92,32 @@ exports.socketChatOn = function(app) {
             }
         }
     }
-
+    let fs=require('fs');
     app.chatSIO.on('connection', function(socket) {
         // socket.emit('chat message', 'socket connet');
         socket.emit('systemMessage', '系統訊息:連線成功');
         console.log('a user connected');
 
+        fs.readFile('G:\\Server\\MagicServer\\MagicServer\\1.png', function(err, buf) {
+            // it's possible to embed binary data
+            // within arbitrarily-complex objects
+            console.log('-----------------');
+            console.log(err);
+            console.log('-----------------');
+            console.log(buf);
+            socket.emit('chatImage', buf);
+            socket.emit('chatMessage', '連線成功!好想555555555555555555555555555555發財!\n連線成功!好想發財!\n連線成功!好想發財!\n連線成功!好想發財!\n連線成功!好想發財!');
+            socket.emit('chatMessage', '連線成功!好222222222222222222222222222!\n連線成功!好想發財!');
+            // socket.emit('image', { image: true, buffer: buf });
+        });
+
         socket.on('findType', function(msg) {
-            console.log('find message: '+msg);
-            if ((!isSocketExistInArray(socket, app.chatSIO.boyFindGirlWait))&&msg=='boyFindGirl') {
+            console.log('find message: ' + msg);
+            if ((!isSocketExistInArray(socket, app.chatSIO.boyFindGirlWait)) && msg == 'boyFindGirl') {
                 app.chatSIO.boyFindGirlWait.push(socket);
-            }
-            else if ((!isSocketExistInArray(socket, app.chatSIO.girlFindBoyWait))&&msg=='girlFindBoy') {
+            } else if ((!isSocketExistInArray(socket, app.chatSIO.girlFindBoyWait)) && msg == 'girlFindBoy') {
                 app.chatSIO.girlFindBoyWait.push(socket);
-            }
-             else if ((!isSocketExistInArray(socket, app.chatSIO.boyFindBoyWait))&&msg=='boyFindBoy') {
+            } else if ((!isSocketExistInArray(socket, app.chatSIO.boyFindBoyWait)) && msg == 'boyFindBoy') {
                 app.chatSIO.boyFindBoyWait.push(socket);
             }
         });
@@ -121,6 +132,21 @@ exports.socketChatOn = function(app) {
             //socket.broadcast.emit('chat message', msg);
             // app.chatSIO.in('chat').emit('chat message', msg);
         });
+
+        socket.on('chatImage', function(imageByteArray) {
+            console.log(socket.id);
+            console.log('chatImage');
+            console.log(socket.rooms);
+            for (let room in socket.rooms) {
+                socket.to(room).broadcast.emit('chatImage', imageByteArray);
+            }
+            socket.emit('chatMessage', '你傳送的圖片是:');
+            socket.emit('chatImage', imageByteArray);
+
+            //socket.broadcast.emit('chat message', msg);
+            // app.chatSIO.in('chat').emit('chat message', msg);
+        });
+
         socket.on('disconnect', function() {
             console.log('user disconnected');
             leaveWaitRoomForAllWaitSocket(socket);
